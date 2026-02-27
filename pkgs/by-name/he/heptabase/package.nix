820a6d76630dc88dd3e18508997a1e78b56ddcb6
cjw <cwentsai@gmail.com>
2026-02-27 23:30:14 +0800

    Update Heptabase to 1.84.2

--- pkgs/by-name/he/heptabase/package.nix
+++ pkgs/by-name/he/heptabase/package.nix
@@ -5,10 +5,10 @@
 }:
 let
   pname = "heptabase";
-  version = "1.84.0";
+  version = "1.84.2";
   src = fetchurl {
     url = "https://github.com/heptameta/project-meta/releases/download/v${version}/Heptabase-${version}.AppImage";
-    hash = "sha256-sk9YN2vNr9jiGzVQOcst+oRLEYjEaZO4nGgD8TxdfIc=";
+    hash = "sha256-Y6gf0R0cRs88dmP5OkwmZZRRFjgzMxYcg2ruledAIDA=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
